4c3c334310f30bf46dacf191824de841b7a2a768
Tage <tage.hisdal@gmail.com>
2022-01-20 19:19:54 +0100

    Added Guy kata

---
+++ Guy/Guy/Guy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guy
+{
+    internal class Guy
+    {
+        private string name;
+        private int cash;
+
+        public Guy(string name, int cash)
+        {
+            this.name = name;
+            this.cash = cash;
+        }
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(name + " has " + cash + " dollar");
+        }
+
+        public int GiveCash(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine("Are you kidding me? "+ amount + " dollar");
+                return 0;
+            }
+            if(amount > cash)
+            {
+                Console.WriteLine("Its to much");
+                return 0;
+            }
+
+            cash -= amount;
+            return amount;
+        }
+
+        public void ReciveCash(int resivedCash)
+        {
+            if(resivedCash <= 0)
+            {
+                Console.WriteLine("Are you kidding me, thats nothing");
+            }
+            else
+            {
+                cash += resivedCash;
+            }
+        }
+
+
+        public string Name { get; set; }
+        public int Cash {
+            get { return cash; } 
+            
+            set { cash += cash; } }
+    }
+}
---
+++ Guy/Guy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Guy;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Guy Joe = new Guy("Joe", 100);
+
+        Joe.WriteMyInfo();
+
+        Joe.GiveCash(-20);
+
+        Joe.ReciveCash(910);
+        Joe.WriteMyInfo();
+
+
+    }
+}
