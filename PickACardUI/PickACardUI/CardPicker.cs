77055464962863956ea2139475ab7ad69b3ba363
Tage <tage.hisdal@gmail.com>
2022-01-20 18:25:52 +0100

    Added UI to PickRandomCard

---
+++ PickACardUI/PickACardUI/CardPicker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for(int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+
+            if (value == 1) return "Spades";
+
+            if (value == 2) return "Harts";
+
+            if (value == 3) return "Clubs";
+
+            return "Dimonds";
+        }
+
+        private static string RandomValue()
+        {
+
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
