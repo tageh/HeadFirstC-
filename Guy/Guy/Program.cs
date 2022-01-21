d245a192cdebc00f57168753fc92bd09f2333076
Tage <tage.hisdal@gmail.com>
2022-01-21 15:50:10 +0100

    Finished Guy class

--- Guy/Guy/Program.cs
+++ Guy/Guy/Program.cs
@@ -6,15 +6,44 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Random random = new Random();
+        double odds = .75;
+
         Guy Joe = new Guy("Joe", 100);
+        Console.WriteLine("Welcome to the casino, The odds are " + odds);
 
-        Joe.WriteMyInfo();
+        while (Joe.Cash > 0)
+        {
+            Joe.WriteMyInfo();
 
-        Joe.GiveCash(-20);
+            Console.WriteLine("How much to you want to bet? ");
+            string howMuch = Console.ReadLine();
 
-        Joe.ReciveCash(910);
-        Joe.WriteMyInfo();
+            if (int.TryParse(howMuch, out int amount))
+            {
 
+                int pot = Joe.GiveCash(amount) * 2;
 
+                if (pot > 0)
+                {
+                    if (random.NextDouble() > odds)
+                    {
+                        int winnings = pot;
+                        Console.WriteLine("You win " + winnings);
+                        Joe.ReciveCash(winnings);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad luck, you lose!");
+                    }
+                }
+
+            }else
+            {
+                Console.WriteLine("Enter a valid number!");
+            }
+
+        }
+        Console.WriteLine("The house always wins!!");
     }
 }
