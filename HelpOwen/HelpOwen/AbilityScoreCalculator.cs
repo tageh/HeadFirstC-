f3ac8b8c5c03115193da540fc77b09c7fd7b7143
Tage <tage.hisdal@gmail.com>
2022-01-27 16:32:27 +0100

    Helping Owen

---
+++ HelpOwen/HelpOwen/AbilityScoreCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpOwen
+{
+    class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DevideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+
+
+        public void CalculateAbilityScore()
+        {
+            double devided = RollResult / DevideBy;
+
+            int added = AddAmount + (int)devided;
+
+            if(added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+    }
+}
