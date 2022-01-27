4c03be1153e9a7efcd5eb526e4b91ce737415ef6
Tage <tage.hisdal@gmail.com>
2022-01-27 18:58:28 +0100

    Elephant class

---
+++ Elephants/Elephants/Elephant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elephants
+{
+    class Elephant
+    {
+        private int _earSize;
+        private string _name;
+
+        public Elephant(string name, int earSize)
+        {
+            this._name = name;
+            this._earSize = earSize;
+        }
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + _name + ".");
+            Console.WriteLine("My ears are " + _earSize + " inches tall.");
+        }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine(_name + " heard a message");
+            Console.WriteLine(whoSaidIt._name + " said this: " + message);
+        }
+
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
+
+        public int EarSize
+        {
+            get { return _earSize; }
+            set { _earSize = value; }
+        }
+        public string Name {
+            get { return _name; }
+            set { _name = value; } 
+        }
+    }
+}
---
+++ Elephants/Elephants/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace Elephants;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Elephant lucinda = new Elephant("Lucinda", 33);
+        Elephant lloyd = new Elephant("Lloyd", 40);
+        Elephant lina = new Elephant("Lina", 30);
+        
+        Elephant[] elephants = new Elephant[] { lloyd, lucinda, lina };
+
+        Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 tw swap, 4 to do something else, 5 to speak, 6 for extra");
+
+        while (true)
+        {
+            char input = Console.ReadKey(true).KeyChar;
+            Console.WriteLine("You pressed " + input);
+            if(input == '1')
+            {
+                Console.WriteLine("Calling lloyd.WhoAmI()");
+                lloyd.WhoAmI();
+            }else if(input == '2')
+            {
+                Console.WriteLine("Calling lucinda.WhoAmI()");
+                lucinda.WhoAmI();
+            }else if (input == '3')
+            {
+                Elephant holder;
+                holder = lloyd;
+                lloyd = lucinda;
+                lucinda = holder;
+                Console.WriteLine("References has been swapped");
+            }else if(input == '4')
+            {
+                lloyd = lucinda;
+                lloyd.EarSize = 4321;
+                lloyd.WhoAmI();
+            }else if(input == '5')
+            {
+                lucinda.SpeakTo(lloyd, "Helloo mr.Lloyd");
+            }else if(input == '6')
+            {
+                PrintElephant(elephants);
+
+                Console.WriteLine("Who are you? ");
+                string fromName = Console.ReadLine();
+                int num1 = Convert.ToInt32(fromName);
+
+                Console.WriteLine("Who do you want to speak to? ");
+                string toName = Console.ReadLine();
+                int num2 = Convert.ToInt32(toName);
+
+                Elephant fromElephant = elephants[num1]; 
+                Elephant toElephant = elephants[num2];
+                Console.WriteLine("What is your message? ");
+                string messageTo = Console.ReadLine();
+                fromElephant.SpeakTo(toElephant, messageTo);
+            }
+            /*Ugly code, but it works xD*/
+            else return;
+            Console.WriteLine();
+        }
+    }
+
+    static void PrintElephant(Elephant[] elephants)
+    {
+         Console.WriteLine("Elephants to choose from");
+         for(int i = 0; i < elephants.Length; i++)
+         {
+            Console.WriteLine(elephants[i].Name+ " " + i);
+         } 
+    }
+}
